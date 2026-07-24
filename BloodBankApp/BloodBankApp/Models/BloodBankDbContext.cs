58919cdace17db84c88515825bab0d254d3b87e5
SohihWahid <140302133+SohihWahid@users.noreply.github.com>
2026-07-24 23:52:39 +0600

    Add files via upload

---
+++ BloodBankApp/BloodBankApp/Models/BloodBankDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBankApp.Models;
+
+public partial class BloodBankDbContext : DbContext
+{
+    public BloodBankDbContext()
+    {
+    }
+
+    public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BloodBankDB;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.DonationDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup).HasMaxLength(5);
+            entity.Property(e => e.City).HasMaxLength(50);
+            entity.Property(e => e.ContactNo).HasMaxLength(20);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.LastDonationDate).HasColumnType("datetime");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
